b38e0053ca813b71974aa0f555470217ec019863
Wellington Previatti <wpreviatti@outlook.com>
2022-03-16 18:50:30 -0700

    Commit parcial das alterações.

---
+++ MicrosWPSShopping.IdentityServer/Model/Context/SQLContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicrosWPSShopping.IdentityServer.Model.Context
+{
+    public class SQLContext : IdentityDbContext
+    {
+        public SQLContext() { }
+        public SQLContext(DbContextOptions<SQLContext> options) : base(options) { }
+
+       
+    }
+}
